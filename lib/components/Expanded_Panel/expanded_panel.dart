e2da9500c948b47761f4938548d54512a8d1ed57
Dare Oloruntoba <64743870+dretoba91@users.noreply.github.com>
2024-02-15 02:07:00 +0100

    add expanded panel folder

---
+++ lib/components/Expanded_Panel/expanded_panel.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyExpandedPanel extends StatefulWidget {
+  const MyExpandedPanel({super.key});
+
+  @override
+  State<MyExpandedPanel> createState() => _MyExpandedPanelState();
+}
+
+class _MyExpandedPanelState extends State<MyExpandedPanel> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
